45601af44b4f14618bc463703f904d20672e224d
Zoirbek <@m.zoirbek>
2024-12-04 09:07:26 +0500

    Initial Commit

---
+++ SwiftUiMap/SwiftUiMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiMapApp.swift
+//  SwiftUiMap
+//
+//  Created by Zoirbek Muxtorov on 04/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
